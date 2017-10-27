c274a4f32dd6d0120c8e79dc39a3a9781f1702f2
Kyle <kyflscout@gmail.com>
2017-10-27 15:29:41 -0600

    Create portscanner.py

---
+++ portscanner.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+import socket
+import subprocess
+import sys
+from datetime import datetime
+import getopt
+
+def main(argv):
+	# Clear the screen
+	subprocess.call('clear', shell=True)
+	print argv
+	# Ask for input
+	remoteServer    = raw_input("Enter a remote host to scan: ")
+	portRange    = raw_input("Enter the port range ending number: ")
+	remoteServerIP  = socket.gethostbyname(remoteServer)
+
+	# Print a nice banner with information on which host we are about to scan
+	print "-" * 60
+	print "Please wait, scanning remote host", remoteServerIP
+	print "-" * 60
+
+	# Check what time the scan started
+	t1 = datetime.now()
+
+	if argv[0] == "tcp":
+		print "tcpscan"
+		try:
+		    for port in range(1,int(portRange)):  
+		        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		        result = sock.connect_ex((remoteServerIP, port))
+		        if result == 0:
+		            print "Port {}: 	 Open".format(port)
+		        sock.close()
+
+		except KeyboardInterrupt:
+		    print "You pressed Ctrl+C"
+		    sys.exit()
+
+		except socket.gaierror:
+		    print 'Hostname could not be resolved. Exiting'
+		    sys.exit()
+
+		except socket.error:
+		    print "Couldn't connect to server"
+		    sys.exit()
+
+	if argv[0] == "udp":
+		try:
+		    for port in range(1,int(portRange)):  
+		        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		        sock.settimeout(5)
+		        result = sock.connect_ex((remoteServerIP, port))
+		        data = sock.recv(2048)
+		        if len(data) > 0:
+		            print "Port {}: 	 Open".format(port)
+		        sock.close()
+
+		except KeyboardInterrupt:
+		    print "You pressed Ctrl+C"
+		    sys.exit()
+
+		except socket.gaierror:
+		    print 'Hostname could not be resolved. Exiting'
+		    sys.exit()
+
+		except socket.error:
+		    print "Couldn't connect to server"
+		    sys.exit()
+
+
+	# Checking the time again
+	t2 = datetime.now()
+
+	# Calculates the difference of time, to see how long it took to run the script
+	total =  t2 - t1
+
+	# Printing the information to screen
+	print 'Scanning Completed in: ', total
+
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:]) 
